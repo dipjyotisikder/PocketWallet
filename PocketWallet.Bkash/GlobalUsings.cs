a83a518f56658e8e48bb67bd9eded03c38c79fd1
Dipjyoti Sikder <dipjyotisikder@gmail.com>
2025-10-22 02:26:31 +0600

    Upgraded the version from 1 to 6 matching with .NET versioning and added a common library

--- PocketWallet.Bkash/GlobalUsings.cs
+++ PocketWallet.Bkash/GlobalUsings.cs
@@ -1,4 +1,4 @@
 ﻿global using Newtonsoft.Json;
 global using PocketWallet.Bkash.Abstraction;
 global using PocketWallet.Bkash.Models;
-global using CONSTANTS = PocketWallet.Bkash.Constants.Constants;
+global using CONSTANTS = PocketWallet.Bkash.Common.Constants.Constants;
